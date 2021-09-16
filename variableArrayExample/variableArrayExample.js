714c61f8e140ca2daab9cd9b946dc7d22bcad59f
PankajiBirds <pankajgoyal.ibirdscollege@gmail.com>
2021-09-16 12:26:21 +0530

    16-09-2021

---
+++ variableArrayExample/variableArrayExample.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+
+export default class VariableArrayExample extends LightningElement {
+    name = "iBirds Services";
+    email = "pankaj.g@ibirdsservices.com";
+    phone = "6350624969";
+}
